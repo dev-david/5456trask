1d57f69f9497e1b0eda70f010943e05b5123d02d
david <simpleaznx@gmail.com>
2013-09-10 22:49:11 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Team Fast Agents | Listing</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600" rel="stylesheet" type="text/css" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<link rel="stylesheet" href="/css/jquery.fancybox.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="/js/jquery.fancybox.pack.js"></script>
+		<script type="text/javascript">
+		$(document).ready(function()
+		{	
+			$('.fancybox').fancybox( { closeClick : 'true' } );
+		});
+		</script>
+
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<div id="header" class="skel-panels-fixed">
+
+				<div class="top">
+
+					<!-- Logo -->
+						<div id="logo">
+							<span class="image avatar48"><img src="images/teamfastagenticon.png" alt="" /></span>
+							<h1 id="title">Team Fast Agents</h1>
+							<span class="byline">YOUR personal team of professionals</span>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="#top" id="top-link" class="skel-panels-ignoreHref"><span class="icon icon-home">Intro</span></a></li>
+								<li><a href="#portfolio" id="portfolio-link" class="skel-panels-ignoreHref"><span class="icon icon-th">Listing Services</span></a></li>
+								<li><a href="#about" id="about-link" class="skel-panels-ignoreHref"><span class="icon icon-dollar">Market Analysis</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-envelope">Contact</span></a></li>
+							</ul>
+						</nav>
+						
+				</div>
+				
+				<div class="bottom">
+
+					<!-- Social Icons -->
+						<ul class="icons">
+							<li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>
+							<li><a href="#" class="icon icon-facebook"><span>Facebook</span></a></li>
+							<li><a href="#" class="icon icon-youtube"><span>Youtube</span></a></li>
+							<li><a href="mailto:info@teamfastagents.com" class="icon icon-envelope"><span>Email</span></a></li>
+						</ul>
+				
+				</div>
+			
+			</div>
+
+		<!-- Main -->
+			<div id="main">
+			
+				<!-- Intro -->
+					<section id="top" class="one">
+						<div class="container">
+
+							<img class="headerlogo" src="images/teamfastagents.jpg" alt="" />
+
+							<header>
+								<h2 class="alt">Thinking about selling <span class="blackblock"> 5456 Trask St? </span> Let us help! This is <span class="blackblock"> YOUR </span> personal team of professionals. Whether you have questions about purchasing or selling, you can rest assured when you have your own Team of Fast Agents backing you.</h2>
+							</header>
+							
+						<!-- 	<p>Ligula scelerisque justo sem accumsan diam quis. Vitae natoque dictum 
+							etiam semper magnis enim feugiat convallis convallis egestas rhoncus ridiculus 
+							in quis risus curabitur tempor. Orci penatibus quisque laoreet condimentum 
+							sollicitudin accumsan elementum.</p> -->
+
+							<footer>
+								<a href="#portfolio" class="button scrolly">More info</a>
+							</footer>
+
+						</div>
+					</section>
+					
+				<!-- Portfolio -->
+					<section id="portfolio" class="two">
+						<div class="container">
+					
+							<header>
+								<h2 class="blackblock">Listing Services</h2>
+							</header>
+							
+							<p>When you list your home with Team Fast Agents, you receive a full blown enterprise level marketing plan.</p>
+						
+							<div class="row">
+								<div class="4u">
+									<article class="item">
+										<a href="images/trask.jpg" class="image full fancybox"><img src="images/trask.jpg" alt="" /></a>
+										<header>
+											<h3><strong>Custom internet domain</strong></h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="#" class="image full"><iframe class="image full" src="http://www.haikudeck.com/e/J3EjpwRZVt" height="301" width="640" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe></a>
+										<header>
+											<h3>The latest presentation solutions</h3>
+										</header>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/trask2.png" class="image full fancybox"><img src="images/cam2.jpg" alt="" /></a>
+										<header>
+											<h3>Professional photography</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="images/printad2.png" class="image full fancybox"><img src="images/printad2.png" alt="" /></a>
+										<header>
+											<h3>Print ads in multiple leading Vietnamese and Chinese language magazines</h3>
+
+										</header>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="" class="image full"><img src="images/pic06.jpg" alt="" /></a>
+										<header>
+											<h3>Full market exposure</h3>
+										</header>
+									</article>
+									<article class="item">
+										<iframe class="image full" width="560" height="315" src="//www.youtube.com/embed/aAs99g4kTMA" frameborder="0" allowfullscreen></iframe>
+										<header>
+											<h3>Video tours</h3>
+										</header>
+									</article>
+								</div>
+							</div>
+
+						</div>
+					</section>
+
+				<!-- About Me -->
+					<section id="about" class="three">
+						<div class="container">
+
+							<header>
+								<h2 style="color: #fff;">Comparative Market Analysis</h2>
+								<h3 style="color: #ecf0f1;">Properties sold within area</h3>
+							</header>
+							<div class="row">
+								<div class="4u">
+									<article class="item">
+										<a href="images/trask.jpg" class="image full fancybox"><img src="images/trask.jpg" alt="" /></a>
+										<header>
+										<h3><strong>1234 Trask St</strong></h3>
+										<ul>
+											<li>3 Bed - 1 Bath</li>
+											<li>1,100 Sq Ft - 4,500 Lot</li>
+											<li>30 days on market</li>
+											<li>SOLD $210,000</li>
+										</ul>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/trask.jpg" class="image full fancybox"><img src="images/trask.jpg" alt="" /></a>
+										<header>
+										<h3><strong>1234 Trask St</strong></h3>
+										<ul>
+											<li>3 Bed - 1 Bath</li>
+											<li>1,100 Sq Ft - 4,500 Lot</li>
+											<li>30 days on market</li>
+											<li>SOLD $210,000</li>
+										</ul>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/trask.jpg" class="image full fancybox"><img src="images/trask.jpg" alt="" /></a>
+										<header>
+										<h3><strong>1234 Trask St</strong></h3>
+										<ul>
+											<li>3 Bed - 1 Bath</li>
+											<li>1,100 Sq Ft - 4,500 Lot</li>
+											<li>30 days on market</li>
+											<li>SOLD $210,000</li>
+										</ul>
+									</article>
+								</div>
+							</div>
+								</header>
+							</article>
+						</div>
+					</section>
+			
+				<!-- Contact -->
+					<section id="contact" class="four">
+						<div class="container">
+
+							<header>
+								<h2>Contact</h2>
+							</header>
+
+							<p>Give us a call! <span class="blackblock">510.545.6932</span></p>
+							
+							<!-- <form method="post" action="#">
+								<div class="row half">
+									<div class="6u"><input type="text" class="text" name="name" placeholder="Name" /></div>
+									<div class="6u"><input type="text" class="text" name="email" placeholder="Email" /></div>
+								</div>
+								<div class="row half">
+									<div class="12u">
+										<textarea name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<a href="#" class="button submit">Send Message</a>
+									</div>
+								</div>
+							</form> -->
+
+						</div>
+					</section>
+			
+			</div>
+
+		<!-- Footer -->
+			<div id="footer" style="background-color: #2c3e50; color: #000;">
+				
+				<!-- Copyright -->
+					<div class="copyright">
+						<p>&copy; 2013 Team Fast Agents. All rights reserved.</p>
+						<ul class="menu">
+							<li>by <a href="http://devdavid.com">DevDavid</a></li>
+						</ul>
+					</div>
+				
+			</div>
+
+	</body>
+</html>
